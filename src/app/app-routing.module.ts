a2097512d3f5ee6ba26a8f248c0104105bbe4b64
tregor722 <tregor1990@gmail.com>
2021-11-14 02:36:27 +0100

    define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import {AboutComponent} from "./about/about.component";
+import{ContactComponent} from"./contact/contact.component";
 const routes: Routes = [
-// {path : 'frirst-url', component: FirstComponent },
-// { path:' second-url', Component: SecondComponenet }
+ {path : 'about', component: AboutComponent  },
+ { path:'contact', component: ContactComponent },
 ];
 
 @NgModule({
