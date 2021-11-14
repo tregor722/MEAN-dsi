9a7ea32006fa016bf70f0aa1e1c74330e964d7e2
tregor722 <tregor1990@gmail.com>
2021-11-14 16:34:08 +0100

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./about/about.component";
 import{ContactComponent} from"./contact/contact.component";
+ import{HomeComponent} from"./home/home.component";
 const routes: Routes = [
  {path : 'about', component: AboutComponent  },
  { path:'contact', component: ContactComponent },
+ { path:'home', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
