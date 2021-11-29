9c969d1cc332b74c421c11ec387e73e2e15108ee
tregor722 <tregor1990@gmail.com>
2021-11-30 00:25:16 +0100

    maison-detail

--- src/app/maisons/maisons.module.ts
+++ src/app/maisons/maisons.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MaisonsRoutingModule } from './maisons-routing.module';
 import { MaisonsComponent } from './maisons.component';
-import { MaisonComponent } from './maison/maison.component';
 
 
 @NgModule({
   declarations: [
     MaisonsComponent,
-    MaisonComponent
+  
   ],
   imports: [
     CommonModule,
