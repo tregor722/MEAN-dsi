2d882d3fffbe9b38c7bcf61dae0ad88eae58aa4c
tregor722 <tregor1990@gmail.com>
2022-01-03 08:55:06 +0100

    login, crud, espace admin et espace client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,18 @@ import { TerrainComponent } from './terrains/terrain/terrain.component';
 import { TerrainsComponent } from './terrains/terrains.component';
 
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ConsulterComponent } from './terrains/consulter/consulter.component';
+
 const routes: Routes = [
  { path:'home', component: HomeComponent },
  { path:'maison', component: MaisonsComponent },
  { path:'terrain', component: TerrainsComponent },
  { path:'', component: HomeComponent },
+ { path:'consultert', component: ConsulterComponent },
  
  { path: 'maisons', loadChildren: () => import('./maisons/maisons.module').then(m => m.MaisonsModule) },
  { path: 'terrains', loadChildren: () => import('./terrains/terrains.module').then(m => m.TerrainsModule) },
+ 
  { path: 'about', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
  { path: '**', component: NotFoundComponent },
 ];
